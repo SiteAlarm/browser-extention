af07f83d814f0f6351968b10ca90f588ce0613ac
Pavel F. <pofilippov@gmail.com>
2026-08-20 01:55:58 +0300

    change site`s URLs

--- src/content/xpath-service.js
+++ src/content/xpath-service.js
@@ -1,7 +1,6 @@
 document.querySelector('body').setAttribute('sa-extention', true);
 
 document.addEventListener('click', async function(event) {
-    console.log(event.target);
     if (event.target.id === 'sa-extention-show-btn') {
         var data = event.target.getAttribute('data-xpath');
         data = JSON.parse(data);
